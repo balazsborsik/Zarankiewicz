28f01d35ad9803ca5603662633443c47b2f09acb
Borsik Balázs <baborsik@gmail.com>
2025-11-15 22:47:33 +0100

    result of run_node_reduction

--- scripts/reset_node_reduction.bat
+++ scripts/reset_node_reduction.bat
@@ -1,11 +1,14 @@
 @echo off
-set "test1_KST=K22"
-set "test2_KST=K22"
+set "test1_KST=K36"
+set "test2_KST=%test1_KST%"
 
 set "test1_DIR=%~dp0..\experiments\nodetest1output\%test1_KST%"
 set "test2_DIR=%~dp0..\experiments\nodetest2output\%test2_KST%"
 
 set "destination_DIR=%~dp0..\output"
+
+call copy_no_replace.bat "%test1_DIR%\graphs" "%destination_DIR%\%test1_KST%\graphs"
+call copy_no_replace.bat "%test2_DIR%\graphs" "%destination_DIR%\%test2_KST%\graphs"
 
 rmdir /s /q "%test1_DIR%"
 mkdir "%test1_DIR%\graphs"
@@ -16,7 +19,5 @@ mkdir "%test2_DIR%\graphs"
 call copy_no_replace.bat "%destination_DIR%\%test1_KST%\graphs" "%test1_DIR%\graphs"
 call copy_no_replace.bat "%destination_DIR%\%test2_KST%\graphs" "%test2_DIR%\graphs"
 
-call .\util\shuffle_rows.exe "%destination_DIR%\%test2_KST%\graphs"
-
 call cleanup_output.bat
-call .\util\get_results.exe output\K22\graphs experiments\current_results.txt
+call .\util\get_results.exe "%destination_DIR%\%test1_KST%\graphs" "%~dp0..\experiments\current_results.txt"
