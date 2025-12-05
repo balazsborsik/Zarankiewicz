6e02a9f85cb77a97d89f84635fce094de063f6a7
Borsik Balázs <baborsik@gmail.com>
2025-12-05 22:03:05 +0100

    Rename OldDynp to NewDynp and update header guards accordingly

--- include/probability/NewDynp.h
+++ include/probability/NewDynp.h
@@ -1,14 +1,14 @@
-#ifndef OLD_DYNP_H
-#define OLD_DYNP_H
+#ifndef NEW_DYNP_H
+#define NEW_DYNP_H
 
 #include "probability/Probabilities.h"
 
-class OldDynp : public Probabilities
+class NewDynp : public Probabilities
 {
  public:
-  OldDynp(int m, int n, int s, int t) : Probabilities(m, n, s, t) {}
-  OldDynp(const Graph& graph, int s, int t) : Probabilities(graph, s, t) {}
+  NewDynp(int m, int n, int s, int t) : Probabilities(m, n, s, t) {}
+  NewDynp(const Graph& graph, int s, int t) : Probabilities(graph, s, t) {}
   double get_p(int v_m, int v_n) override;
 };
 
-#endif  // OLD_DYNP_H
+#endif  // NEW_DYNP_H
