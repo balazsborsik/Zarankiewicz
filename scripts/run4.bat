657cc0d737d3126126612b1a3fbcab9bed9f9c7f
Borsik Balázs <baborsik@gmail.com>
2025-12-06 22:34:12 +0100

    NewDynp tests

--- scripts/run4.bat
+++ scripts/run4.bat
@@ -2,7 +2,7 @@ set "test1_KST=K22"
 set "test2_KST=K22"
 set "test3_KST=K22"
 set "test4_KST=K22"
-
+goto comment
 copy ..\output\%test1_KST%\graphs\* ..\experiments\test1output\%test1_KST%\graphs\
 copy ..\output\%test2_KST%\graphs\* ..\experiments\test2output\%test2_KST%\graphs\
 copy ..\output\%test3_KST%\graphs\* ..\experiments\test3output\%test3_KST%\graphs\
@@ -12,7 +12,7 @@ copy ..\output\%test4_KST%\graphs\* ..\experiments\test4output\%test4_KST%\graph
 ..\scripts\util\shuffle_graphs.exe ..\experiments\test2output\%test2_KST%\graphs
 ..\scripts\util\shuffle_graphs.exe ..\experiments\test3output\%test3_KST%\graphs
 ..\scripts\util\shuffle_graphs.exe ..\experiments\test4output\%test4_KST%\graphs
-
+:comment
 start "test1" cmd /k "cd .. && .\bin\program.exe test1 && .\scripts\util\compare experiments\test1output\%test1_KST%\results\finalresults0.txt output\%test1_KST%\current_results.txt"
 start "test2" cmd /k "cd .. && .\bin\program.exe test2 && .\scripts\util\compare experiments\test2output\%test2_KST%\results\finalresults0.txt output\%test2_KST%\current_results.txt
 start "test3" cmd /k "cd .. && .\bin\program.exe test3 && .\scripts\util\compare experiments\test3output\%test3_KST%\results\finalresults0.txt output\%test3_KST%\current_results.txt"
