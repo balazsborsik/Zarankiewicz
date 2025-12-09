0a73593bdd98b9ca6d0b74aa04d7bdbce5c5a9f5
Borsik Balázs <baborsik@gmail.com>
2025-12-09 01:04:07 +0100

    one new graph

--- src/probability/NewDynp.cpp
+++ src/probability/NewDynp.cpp
@@ -8,6 +8,7 @@ double NewDynp::get_p(int v_m, int v_n)
       (expected_percent_ * n_ * m_ - edge_count_) / (n_ * m_ - edge_count_);
   double prob_A = (expected_m_ - degree_m_[v_m]) / (n_ - degree_m_[v_m]);
   double prob_B = (expected_n_ - degree_n_[v_n]) / (m_ - degree_n_[v_n]);
-  return ((expected_percent_adjusted + prob_A) / 2.0 + (expected_percent_adjusted + prob_B) / 2.0) /
+  return ((expected_percent_adjusted + 2.0 * prob_A) / 3.0 +
+          (expected_percent_adjusted + 2.0 * prob_B) / 3.0) /
          2.0;
 }
